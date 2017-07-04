e39bf70977c0fae08f9215e4f8d713cb31398dda
IsraeliKyozouTeam <guyb2000@gmail.com>
2017-07-04 12:31:26 +0300

    Add files via upload

---
+++ SearchZipDelete/DataAccessObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchZipDelete
+{
+    class DataAccessObject
+    {
+        FileDataSetTableAdapters.QueriesTableAdapter adapter;
+        public DataAccessObject() {
+            adapter = new FileDataSetTableAdapters.QueriesTableAdapter();
+        }
+        public object getCompanyInfo(int id) {
+            return adapter.GetFilesRowsPerCompanyId(id);             
+        }
+    }
+}
