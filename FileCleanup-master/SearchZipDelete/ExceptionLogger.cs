147bf9c1c8731bd8d95240f55ee9c358e1ec26e6
IsraeliKyozouTeam <guyb2000@gmail.com>
2017-07-04 11:56:54 +0300

    Add files via upload

---
+++ FileCleanup-master/SearchZipDelete/ExceptionLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchZipDelete
+{
+    public class ExceptionLogger
+    {
+
+        HashSet<UnauthorizedAccessException> UnauthorizedAccessLog;
+        HashSet<System.IO.DirectoryNotFoundException> DirectoryNotFoundLog;
+        HashSet<Exception> GeneralExceptionLog;
+
+        public ExceptionLogger() {
+           UnauthorizedAccessLog = new HashSet<UnauthorizedAccessException>();
+           DirectoryNotFoundLog = new HashSet<System.IO.DirectoryNotFoundException>();
+            GeneralExceptionLog = new HashSet<Exception>();
+        }
+        public void addUnauthorizedException(UnauthorizedAccessException e) {
+            UnauthorizedAccessLog.Add(e);
+        }
+        public void addDirectoryException(System.IO.DirectoryNotFoundException e)
+        {
+            DirectoryNotFoundLog.Add(e);
+        }
+        public void addGeneralException(Exception e) {
+            GeneralExceptionLog.Add(e);
+        }
+
+    }
+}
