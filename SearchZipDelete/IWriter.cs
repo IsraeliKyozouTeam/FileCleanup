96aaafd416e8f34eb6055ae610cf28ae4f62de2f
IsraeliKyozouTeam <guyb2000@gmail.com>
2017-07-05 11:14:56 +0300

    Add files via upload

---
+++ SearchZipDelete/IWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SearchZipDelete
+{
+    interface IWriter
+    {
+
+        void AddToWrite(string text);
+
+        void WriteTo(string path);
+
+        void RemoveFromWrite(string text);
+
+       
+    }
+}
---
+++ SearchZipDelete/Writer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SearchZipDelete
+{
+    class Writer : IWriter
+    {
+
+        string textToWrite;
+
+        public Writer(string textToWrite)
+        {
+            this.textToWrite = textToWrite;
+        }
+
+        public void AddToWrite(string text)
+        {
+            textToWrite += text;
+        }
+
+        public void RemoveFromWrite(string text)
+        {
+            textToWrite = Regex.Replace(textToWrite, text, "");
+        }
+
+        public void WriteTo(string path)
+        {
+            StreamWriter writer = new StreamWriter( path );
+            writer.Write(textToWrite);
+            writer.Close();
+        }
+    }
+}
