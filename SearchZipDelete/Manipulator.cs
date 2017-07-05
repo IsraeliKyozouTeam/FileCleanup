c09fb695abc054f5bd470a538d9b895235915079
IsraeliKyozouTeam <guyb2000@gmail.com>
2017-07-05 14:00:40 +0300

    ICleaner

--- SearchZipDelete/Manipulator.cs
+++ SearchZipDelete/Manipulator.cs
@@ -11,7 +11,22 @@ namespace SearchZipDelete
 
         public Searcher searcher { get; protected set; }
         public Writer writer { get;  protected set;}
+        public FileManipulator fileManip { get; protected set; }
+
         
+
+        public Manipulator()
+        {
+            searcher = new Searcher();
+            writer = new Writer();
+            fileManip = new FileManipulator();
+            
+        }
+
+        public void RegisterURL(string url)
+        {
+
+        }
 
 
     }
