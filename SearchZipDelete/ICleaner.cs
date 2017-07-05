90b7d2ecef92e2e5903031dad3fc42c3b6a61681
IsraeliKyozouTeam <guyb2000@gmail.com>
2017-07-05 15:58:15 +0300

    Edited the ICleaner interface

--- SearchZipDelete/ICleaner.cs
+++ SearchZipDelete/ICleaner.cs
@@ -10,8 +10,11 @@ namespace SearchZipDelete
     {
         // Recieves a path to a directory for the cleaner to focus on. This function saves the
         // directory to a property and uses it for the following functions. It returns true if the
-        // dir is valid and false if not.
+        // dir is valid and false if not. UnregisterDirectory unregisters the current dir and tells the
+        // object that the cleaner is no longer in possesion of a registered dir (via bool most likely).
         bool RegisterDirectory(string dir);
+
+        void UnregisterDirectory();
 
         // This function recieves a directory and cleans it by taking all the files
         // identified by the identifier and moves them to the specified dirTo.
--- SearchZipDelete/ReportsManager.cs
+++ SearchZipDelete/ReportsManager.cs
@@ -70,6 +70,8 @@ namespace SearchZipDelete
 
         }
 
+        public Write
+
 
 
 
